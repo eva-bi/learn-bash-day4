291ab8470ee2823ac7ec1d95ed036ad5a44c9467
eva-tuan <eva.tuanla@gmail.com>
2015-01-30 15:36:21 +0700

    fixed unique user id.

--- code/count_users.sh
+++ code/count_users.sh
@@ -3,20 +3,18 @@
 START=$(date +%s)
 for i in $(seq -f "%02g" 1 10)
 do
-	if [ ! -f "log$i.log" ]
+	if [ ! -f "log$i.tar.gz" ]
 		then
 		curl "https://s3-ap-northeast-1.amazonaws.com/aucfan-tuningathon/dataset/result.day$i.log.gz" > "log$i.tar.gz"
-		zcat "log$i.tar.gz" >> "log$i.log"
 		zcat "log$i.tar.gz" >> "log_main.log"
 	fi
 done
 echo "================================================"
 echo "==  The number of unique user for each days   =="
 echo "================================================"
-
 for i in $(seq -f "%02g" 1 10)
 do
-	DAY=$(cut -f 2-3 log$i.log | sort | uniq | wc -l)
+	DAY=$(zcat "log$i.tar.gz" | cut -f 3 | sort | uniq | wc -l)
 	echo "Day $i: " $DAY " users"
 done
 
@@ -25,18 +23,17 @@ echo "================================================"
 echo "== The number of unique user for each regions =="
 echo "================================================"
 COUNTRY=$(cut -f 2 log_main.log | sort | uniq)
-TOTAL=0
 for c in $COUNTRY; 
 do 
 	
 	NUM=$(grep -o $c log_main_uniq.log | wc -l)
 	echo $c " : " $NUM " users"; 
-	TOTAL=$(($TOTAL+$NUM))
 done
 
 echo "================================================"
 echo "====  The number of unique user in total   ====="
 echo "================================================"
+TOTAL=$(cut -f 3 log_main.log | sort | uniq | wc -l)
 echo "Total : " $TOTAL " users"
 END=$(date +%s)
 DIFF=$(( $END - $START ))
